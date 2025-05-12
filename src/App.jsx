84df2fc89a6fde1c707dca22922a65ec38dc36ec
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-05-12 15:22:00 -0400

    create theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 function App() {
   return (
     <>
       <div>Hung</div>
+      <Typography variant="body2" color="text.secondary">
+        Hello world
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
