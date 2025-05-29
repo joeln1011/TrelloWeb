aa3d298516be3139a4b2a7e1701f782e69095036
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-05-29 19:43:41 -0400

    Board Bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -17,16 +17,16 @@ import NotificationsNone from "@mui/icons-material/NotificationsNone";
 import Tooltip from "@mui/material/Tooltip";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import Profiles from "./Menus/Profiles";
-
+import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
 function AppBar() {
   return (
     <Box
-      px={2}
       sx={{
         width: "100%",
         height: (theme) => theme.trello.appBarHeight,
         display: "flex",
         alignItems: "center",
+        paddingX: 2,
         justifyContent: "space-between",
         gap: 2,
         overflowX: "auto",
@@ -63,7 +63,9 @@ function AppBar() {
           <Recent />
           <Starred />
           <Templates />
-          <Button variant="outlined">Create</Button>
+          <Button variant="outlined" startIcon={<LibraryAddIcon />}>
+            Create
+          </Button>
         </Box>
       </Box>
 
