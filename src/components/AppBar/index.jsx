09758883c10150a7afbd4a19138e5ce092b7ed5f
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-05-29 18:40:25 -0400

    App Bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -28,6 +28,8 @@ function AppBar() {
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
+        gap: 2,
+        overflowX: "auto",
       }}
     >
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
@@ -37,6 +39,7 @@ function AppBar() {
             component={TrelloIcon}
             inheritViewBox
             sx={{ color: "primary.main" }}
+            fontSize="small"
           />
           <Typography
             variant="span"
@@ -49,11 +52,19 @@ function AppBar() {
             Trello
           </Typography>
         </Box>
-        <Workspaces />
-        <Recent />
-        <Starred />
-        <Templates />
-        <Button variant="outlined">Create</Button>
+
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            gap: 1,
+          }}
+        >
+          <Workspaces />
+          <Recent />
+          <Starred />
+          <Templates />
+          <Button variant="outlined">Create</Button>
+        </Box>
       </Box>
 
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
@@ -62,15 +73,18 @@ function AppBar() {
           label="Search..."
           type="search"
           size="small"
+          sx={{ minWidth: 120 }}
         />
         <ModeSelect />
         <Tooltip title="Notifications" arrow>
           <Badge color="secondary" variant="dot" sx={{ cursor: "pointer" }}>
-            <NotificationsNone />
+            <NotificationsNone sx={{ color: "primary.main" }} />
           </Badge>
         </Tooltip>
         <Tooltip title="Help" arrow>
-          <HelpOutlineOutlinedIcon sx={{ cursor: "pointer" }} />
+          <HelpOutlineOutlinedIcon
+            sx={{ cursor: "pointer", color: "primary.main" }}
+          />
         </Tooltip>
         <Profiles />
       </Box>
--- src/theme.js
+++ src/theme.js
@@ -14,11 +14,61 @@ const theme = createTheme({
         secondary: deepOrange,
       },
     },
-    dark: {
-      palette: {
-        mode: "dark",
-        primary: cyan,
-        secondary: orange,
+    dark: true,
+    palette: {
+      primary: cyan,
+      secondary: orange,
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          "*::-webkit-scrollbar": {
+            width: "8px",
+            height: "8px",
+          },
+          "*::-webkit-scrollbar-thumb": {
+            backgroundColor: "#bdc3c7",
+            borderRadius: "8px",
+          },
+          "*::-webkit-scrollbar-thumb:hover": {
+            backgroundColor: "#00b894",
+            borderRadius: "8px",
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: "0.875rem",
+        }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: "0.875rem",
+          ".MuiOutlinedInput-notchedOutline": {
+            borderColor: theme.palette.primary.light,
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: theme.palette.primary.main,
+          },
+          "& fieldset": {
+            borderWidth: "1px !important",
+          },
+        }),
       },
     },
   },
