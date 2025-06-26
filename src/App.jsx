47530ecf954bd2124b1ca3a935a081d3739cd269
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-06-26 15:46:45 -0400

    Create Login and Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { Routes, Route, Navigate } from "react-router";
 import Board from "~/pages/Boards/_id";
 import NotFound from "~/pages/404/NotFound";
+import Auth from "~/pages/Auth/Auth";
 function App() {
   return (
     <Routes>
+      {/* Redirect Route */}
       <Route
         path="/"
         element={
           <Navigate to="/boards/6855bd3e6ce5899e61d47828" replace={true} />
         }
       />
+      {/* Board Detail */}
       <Route path="/boards/:boardId" element={<Board />} />
+
+      {/* Authentication */}
+      <Route path="/login" element={<Auth />} />
+      <Route path="/register" element={<Auth />} />
 
       {/* 404 not found page */}
       <Route path="*" element={<NotFound />} />
