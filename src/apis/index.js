d689f79f8d2d9352faa5a37e3c71572ad0c8f4c6
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-07-13 23:36:58 -0400

    Function Invite User

--- src/apis/index.js
+++ src/apis/index.js
@@ -113,3 +113,12 @@ export const updateCardDetailsAPI = async (cardId, updateData) => {
   );
   return response.data;
 };
+
+export const inviteUserToBoardAPI = async (data) => {
+  const response = await authorizeAxiosInstance.post(
+    `${API_ROOT}/v1/invitations/board`,
+    data
+  );
+  toast.success('User invited to board successfully!');
+  return response.data;
+};
